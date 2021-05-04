5ac9c1fe9faeef823e58f5d3f8057d0349cd1ed5
Jen Janik <jenmjanik@gmail.com>
2021-05-04 16:55:20 -0500

    junk commit

--- wayfarer/src/app/app.module.ts
+++ wayfarer/src/app/app.module.ts
@@ -6,15 +6,13 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CitiesComponent } from './cities/cities.component';
 import { CarouselBasicComponent } from './carousel-basic/carousel-basic.component';
-import { TopicComponent } from './topic/topic.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     CitiesComponent,
-    CarouselBasicComponent,
-    TopicComponent
+    CarouselBasicComponent
   ],
   imports: [
     BrowserModule,
